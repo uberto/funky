6908107f277adcfe2d6e11874b2d24302d422d5b
uberto <uberto.gama@gmail.com>
2020-04-24 08:03:49 +0100

    fix gradle build

--- funky-core/src/main/kotlin/com/ubertob/funky/nullable/NullableFunctions.kt
+++ funky-core/src/main/kotlin/com/ubertob/funky/nullable/NullableFunctions.kt
@@ -1,7 +1,6 @@
 package com.ubertob.funky.nullable
 
 
-fun <T: Any, U: Any> T?.ifNotNull(f: (T) -> U): U? = TODO()
+fun <T: Any, U: Any> T?.ifNotNull(f: (T) -> U): U? = this?.let(f)
 
-
-fun <U: Any> CharSequence?.ifNotNullOrEmpty(f: (CharSequence) -> U): U? = TODO()
+fun <U: Any> CharSequence?.ifNotNullOrEmpty(f: (CharSequence) -> U): U? = this?.let { if (it.isNotEmpty()) f(it) else null }
