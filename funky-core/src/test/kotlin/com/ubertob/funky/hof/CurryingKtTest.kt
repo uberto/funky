fa952d5b07d119fb7460244de20cbf70108c651d
uberto <uberto.gama@gmail.com>
2020-11-19 10:26:21 +0000

    Fixing currying test

--- funky-core/src/test/kotlin/com/ubertob/funky/hof/CurryingKtTest.kt
+++ funky-core/src/test/kotlin/com/ubertob/funky/hof/CurryingKtTest.kt
@@ -1,6 +1,8 @@
 package com.ubertob.funky.hof
 
 import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
 
 fun start(s: String, x: Int) = s.substring(x)
 fun last(x: Int, s: String) = s.substring(x)
@@ -9,14 +11,40 @@ fun three(s: String, x: Int, c: Char) = s.substring(x) + c
 
 internal class CurryingTest {
 
+
     @Test
-    fun applying() {
-        listOf("bread", "cheese", "steak")
+    fun `applying first parameter`() {
+        val words = listOf("bread", "cheese", "steak")
+                .map(String::toUpperCase)
+                .map(::last applyFirst 2)
+
+        expectThat(words).isEqualTo(listOf("EAD", "EESE", "EAK"))
+    }
+
+    @Test
+    fun `applying last parameter`() {
+        val words = listOf("bread", "cheese", "steak")
                 .map(String::toUpperCase)
                 .map(::start applyLast 2)
-                .map(::last applyFirst 2)
+
+        expectThat(words).isEqualTo(listOf("EAD", "EESE", "EAK"))
+    }
+
+
+    @Test
+    fun `applying receiver parameter`() {
+        val words = listOf("bread", "cheese", "steak")
+                .map(String::toUpperCase)
                 .map(String::startR applyThis 2)
+        expectThat(words).isEqualTo(listOf("EAD", "EESE", "EAK"))
+    }
+
+    @Test
+    fun `applying 3 parameters`() {
+        val words = listOf("bread", "cheese", "steak")
+                .map(String::toUpperCase)
                 .map(::three applyLast3 'a' applyLast 1)
+        expectThat(words).isEqualTo(listOf("READa", "HEESEa", "TEAKa"))
     }
 }
 
