5da9e5e8493b14bf72f9ff50ac3110b0ddd6c295
uberto <uberto.gama@gmail.com>
2021-02-20 16:34:54 +0000

    improved lexer

--- funky-core/src/main/kotlin/com/ubertob/funky/json/JsonLexer.kt
+++ funky-core/src/main/kotlin/com/ubertob/funky/json/JsonLexer.kt
@@ -1,6 +1,30 @@
 class JsonLexer {
 
-    fun tokenize(jsonStr: String): Sequence<String> = generateSequence {
-        jsonStr
+    fun tokenize(jsonStr: String): Sequence<String> =
+        sequence {
+            var currWord = ""
+            jsonStr.forEach {
+                when (it) {
+                    ' ', '\t', '\n', '\r' -> currWord = yieldIfNotEmpty(currWord)
+                    '{', '}', '[', ']', ',', ':' -> {
+                        currWord = yieldIfNotEmpty(currWord)
+                        yield(it.toString())
+                    }
+                    '"' -> {
+                        currWord = yieldIfNotEmpty(currWord)
+                        yield(it.toString())
+                    }
+                    else -> currWord += it
+                }
+            }
+            yieldIfNotEmpty(currWord)
+        }
+
+    private suspend fun SequenceScope<String>.yieldIfNotEmpty(currWord: String): String {
+        if (currWord.isNotEmpty()) {
+            yield(currWord)
+        }
+        return ""
     }
+
 }
--- funky-core/src/test/kotlin/com/ubertob/funky/json/JsonLexerTest.kt
+++ funky-core/src/test/kotlin/com/ubertob/funky/json/JsonLexerTest.kt
@@ -16,4 +16,42 @@ class JsonLexerTest {
 
         expectThat(seq.toList()).isEqualTo(listOf(json))
     }
+
+    @Test
+    fun `spaces tab and new lines word`() {
+        val json = "  abc   def\ngh\tijk\r lmn \n\n opq"
+        val seq = lexer.tokenize(json)
+
+        expectThat(seq.toList()).isEqualTo(
+            listOf(
+                "abc", "def", "gh", "ijk", "lmn", "opq"
+            )
+        )
+    }
+
+    @Test
+    fun `json special tokens`() {
+        val json = "[]{}:,  [a,b,c]  {d:e}"
+        val seq = lexer.tokenize(json)
+
+        expectThat(seq.toList()).isEqualTo(
+            listOf(
+                "[", "]", "{", "}", ":", ",", "[", "a", ",", "b", ",", "c", "]", "{", "d", ":", "e", "}"
+            )
+        )
+    }
+
+    @Test
+    fun `json strings`() {
+        val json = """
+            { "abc": 123}
+        """.trimIndent()
+        val seq = lexer.tokenize(json)
+
+        expectThat(seq.toList()).isEqualTo(
+            listOf(
+                "{", "\"", "abc", "\"", ":", "123", "}"
+            )
+        )
+    }
 }
